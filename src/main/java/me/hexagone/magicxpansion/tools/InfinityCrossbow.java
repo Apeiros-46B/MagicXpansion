029ec13c1bb3e4e3fc651ef7e30873d2c46ce444
HexaGone68 <miajojo.y@gmail.com>
2021-03-26 10:29:52 -0700

    Fixed Crossbow of Infinity behaviour

--- src/main/java/me/hexagone/magicxpansion/tools/InfinityCrossbow.java
+++ src/main/java/me/hexagone/magicxpansion/tools/InfinityCrossbow.java
@@ -8,8 +8,10 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CrossbowMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,51 +24,27 @@ public class InfinityCrossbow extends SlimefunItem {
     }
 
     @EventHandler
-    public void onCrossBowShoot(EntityShootBowEvent e) {
+    public void onCrossBowLoad(PlayerInteractEvent e) {
 
         List<String> lore = new ArrayList<String>();
         lore.add("");
         lore.add("&aNever runs out of arrows.");
 
-        if (Objects.requireNonNull(e.getBow()).getType() == Material.CROSSBOW) {
-            if (e.getBow().hasItemMeta()) {
-                if (Objects.requireNonNull(e.getBow().getItemMeta()).hasLore()) {
-                    if (e.getBow().getItemMeta().getLore() == lore) {
+        Player p = e.getPlayer();
 
-                        Player p = (Player) e.getEntity();
-                        // Player.getInventory();
-                        //ItemStack arrow = p.getInventory().getItem(p.getInventory().first(Material.ARROW));
-                        if (p.getInventory().contains(Material.FIREWORK_ROCKET)) {
-                            ItemStack ammo = p.getInventory().getItem(p.getInventory().first(Material.FIREWORK_ROCKET));
-                            if (ammo != null && ammo.getAmount() != 64) {
-                                ammo.setAmount(ammo.getAmount()+1);
-                            }
-                        } else {
-                            if (p.getInventory().contains(Material.TIPPED_ARROW)) {
-                                ItemStack ammo = p.getInventory().getItem(p.getInventory().first(Material.TIPPED_ARROW));
-                                if (ammo != null && ammo.getAmount() != 64) {
-                                    ammo.setAmount(ammo.getAmount()+1);
-                                }
-                            } else {
-                                if (p.getInventory().contains(Material.SPECTRAL_ARROW)) {
-                                    ItemStack ammo = p.getInventory().getItem(p.getInventory().first(Material.SPECTRAL_ARROW));
-                                    if (ammo != null && ammo.getAmount() != 64) {
-                                        ammo.setAmount(ammo.getAmount()+1);
-                                    }
-                                } else {
-                                    if (p.getInventory().contains(Material.ARROW)) {
-                                        ItemStack ammo = p.getInventory().getItem(p.getInventory().first(Material.ARROW));
-                                        if (ammo != null && ammo.getAmount() != 64) {
-                                            ammo.setAmount(ammo.getAmount()+1);
-                                        }
-                                    } else {
-                                        e.setCancelled(true);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+        if (e.getMaterial() == Material.CROSSBOW) {
+            CrossbowMeta cMeta = (CrossbowMeta) e.getItem().getItemMeta();
+
+            List<ItemStack> projectiles = null;
+            if (cMeta != null) {
+                projectiles = cMeta.getChargedProjectiles();
+            }
+            ItemStack projectileItemStack = projectiles.get(0);
+
+            if (!p.getInventory().contains(Material.AIR)) {
+                p.getInventory().setItem(p.getInventory().firstEmpty(), projectileItemStack.clone());
+            } else {
+                p.sendMessage("&cYou do not have enough inventory space!");
             }
         }
     }
